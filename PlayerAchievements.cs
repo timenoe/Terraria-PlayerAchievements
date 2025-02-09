047d150f8f4d451ab20e23bdbaf3dabf2dcfa281
Tim Long <timlong105@gmail.com>
2025-02-09 09:32:26 -0500

    Initial commit

---
+++ PlayerAchievements.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace PlayerAchievements
+{
+    /// <summary>
+    ///  Mod that adds achievements for player difficulties to the in-game list
+    /// </summary>
+    public class PlayerAchievements : Mod
+	{
+
+	}
+}
